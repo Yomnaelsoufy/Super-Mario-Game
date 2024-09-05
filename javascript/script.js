e2047587e7d7ff7a0e5f664cf0f1b2f64b640606
EJADA\yelsoufy <yelsoufy.ejada@alrajhibank.com.my>
2024-09-05 16:20:38 +0200

    First Simple Super Mario game

---
+++ javascript/script.js
@@ -0,0 +1,69 @@
+const mario = document.querySelector('.mario');
+const obstacle = document.querySelector('.obstacle');
+const restartButton = document.querySelector('#restart-button');
+let isJumping = false;
+let isGameOver = false;
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space' && !isJumping && !isGameOver) {
+        jump();
+    }
+});
+
+function jump() {
+    let position = 100;
+    isJumping = true;
+    
+    const upInterval = setInterval(() => {
+        if (position >= 250) {
+            clearInterval(upInterval);
+
+            const downInterval = setInterval(() => {
+                if (position <= 100) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    mario.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            position += 20;
+            mario.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+function moveObstacle() {
+    let obstaclePosition = 800;
+    
+    const obstacleInterval = setInterval(() => {
+        if (obstaclePosition <= 0) {
+            obstaclePosition = 800;
+        } else {
+            obstaclePosition -= 10;
+        }
+        obstacle.style.left = obstaclePosition + 'px';
+
+        // Collision detection
+       
+        const marioPosition = parseInt(window.getComputedStyle(mario).getPropertyValue('bottom'));
+        if (obstaclePosition > 0 && obstaclePosition < 100 && marioPosition < 150) {
+            clearInterval(obstacleInterval);
+            isGameOver = true;
+            restartButton.style.display = 'block';
+        }
+    }, 20);
+}
+
+function restartGame() {
+    isGameOver = false;
+    restartButton.style.display = 'none';
+    obstacle.style.left = '800px'; 
+    mario.style.bottom = '100px'; 
+    moveObstacle(); 
+}
+
+restartButton.addEventListener('click',restartGame);
+
+moveObstacle();
